4d719650273072530bdb32618f80a20d76617452
Cymiee <avyuktchamria@gmail.com>
2026-06-10 19:07:42 +0530

    added printrow which prints with color

--- display.cpp
+++ display.cpp
@@ -6,14 +6,31 @@
 #include "guess.h"
 #include <string>
 #include <iostream>
+#include <array>
+#include <vector>
 
-#define GRAY "\033[100m"
-#define YELLOW "\033[43m"
-#define GREEN "\033[42m"
-#define RESET  "\033[0m"
+constexpr const char* GRAY = "\033[100m";
+constexpr const char* YELLOW = "\033[43m";
+constexpr const char* GREEN = "\033[42m";
+constexpr const char* RESET = "\033[0m";
 
 void printRow(const std::string& guess, const std::array<LetterResult, 5>& guessScores) {
     for (int i = 0; i < 5; i++) {
-        if ();
+        if (guessScores[i] == LetterResult::Correct) {
+            std::cout << GREEN << guess[i] << RESET;
+        }
+        else if (guessScores[i] == LetterResult::Misplaced) {
+            std::cout << YELLOW << guess[i] << RESET;
+        }
+        else if (guessScores[i] == LetterResult::Wrong) {
+            std::cout << GRAY << guess[i] << RESET;
+        }
+        else {
+            std::cout << guess[i];
+            std::cerr << "Unexpected state, cannot determine color.\n";
+        }
     }
+    std::cout << '\n';
 }
+
+void printBoard(const std::vector<std::vector>)
